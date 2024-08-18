bddde7f5e81a6d09187f0c78db44f7b63f8f99a6
Disguied-Coffee <danielidanque@gmail.com>
2024-08-18 00:14:41 -0500

    Got a 'script' to work!

--- src/app/page.js
+++ src/app/page.js
@@ -15,31 +15,74 @@ import bg from "../../public/np_file_17300.jpeg"
 import Image from "next/image";
 import Content from "./content";
 import Overlay from "@/components/overlay";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect} from "react";
+
+
+const TypingScriptDynamic = ({
+  word
+}) => {
+
+  //https://blog.logrocket.com/5-ways-implement-typing-animation-react/ ;-;
+  
+  let text = word;
+  console.log(text)
+  let delay = 10;
+  
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex < text.length) {
+      const timeout = setTimeout(() => {
+        setCurrentText(prevText => prevText + text[currentIndex]);
+        setCurrentIndex(prevIndex => prevIndex + 1);
+      }, delay);
+  
+      return () => clearTimeout(timeout);
+    }
+  }, [currentIndex, delay, text]);
+
+  return <div className="absolute bottom-[2px] left-[2vw] font-Ubuntu-Mono">{currentText}<span id="trailingChar">{"█"}</span></div>;
+};
+
 
 const Page = ({
   changeHC,
   passRef
 }) => {
+  let getWord = ()=>{
+    let scripts = [
+      'console.log("Hello World");',
+      'System.out.println("Hello World");',
+      'std::cout << "Hello World" << std::endl;',
+      'print("Hello World")',
+      'echo "Hello World"',
+      'Serial.println("Hello World");'
+    ]
+
+    return scripts[Math.round(Math.random() * scripts.length - 1)];
+  };
+
   return (
     <div className="fixed [scroll-snap-align:_start_none] h-[100vh] pt-[0vh] text-white z-[0]">
       <Image className="object-cover h-full w-full" src={bg} alt="hi" />
       {/* actual  */}
       <div className="whitespace-nowrap w-[100vw]">
+        <TypingScriptDynamic word={getWord()}/>
         <div className="absolute top-[30vh] 
                         text-[1.5rem]
                         sm:text-[3.375rem]
                         md:text-[4.375rem] blah-text left-[1vw] ">
           <h1 className="main-text font-[Ubuntu] italic">&quot;Hello World&quot;</h1>
-          <h1 className="w-[90vw] [animation-delay:0.6s_!important] main-text absolute left-[2rem] sm:left-[4rem]">FROM DISGUISED_COFFEE</h1>
+          <h1 className="w-[90vw] [animation-delay:2.7s_!important] main-text absolute left-[2rem] sm:left-[4rem]">FROM DISGUISED_COFFEE</h1>
         </div>
         <div className="absolute left-[2vw] bottom-[16vh] sm:bottom-[11vh] lazycssthing flex flex-col items-center text-center mt-[100vh]">
           <h2 className="text-[1.7rem]">
             <span className="main-captions">Student Programmer,</span><br />
-            <span className="main-captions [animation-delay:2.5s_!important]">and Computer Engineer.</span>
+            <span className="main-captions [animation-delay:4.5s_!important]">and Computer Engineer.</span>
           </h2>
           {/* DO SOMETHING ABOUT THIS. */}
-          <button className="btn mt-[2rem] py-[0.5rem] px-[1.2rem] text-[1.2rem] select-all main-captions [animation-delay:3.1s_!important]"
+          <button className="btn mt-[2rem] py-[0.5rem] px-[1.2rem] text-[1.2rem] select-all main-captions [animation-delay:5s_!important]"
             onClick={
               (event) => {
                 changeHC("about");
